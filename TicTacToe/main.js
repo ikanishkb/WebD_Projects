39f59f40536fdce782191a4d5dd77ad22a2da9ac
unknown <ikanishkb@gmail.com>
2019-06-27 01:44:21 +0530

    Scorekeeper Added

--- TicTacToe/main.js
+++ TicTacToe/main.js
@@ -1,4 +1,9 @@
-var counter = 0
+var counter = 0;
+var tiecount=0;
+var p1score=0;
+var p2score=0;
+var tiescore=0;
+
 function handleClick(id){
     var elem = document.getElementById(id)
     if(elem.innerHTML != "?"){
@@ -9,111 +14,147 @@ function handleClick(id){
         elem.innerHTML = currentPlayer;
         counter++;
         won();
+        tie();
+        scoreupdate();
     }
 }
 
-const winCombos = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [1, 5, 9],
-    [3, 5, 7],
-    [1, 4, 7],
-    [2, 5, 8],
-    [3, 6, 9],
-  ]
+//      winCombos
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9],
+//     [1, 5, 9],
+//     [3, 5, 7],
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [3, 6, 9],
 
 function won(){
     if(document.getElementById("1").innerHTML == "X" &&
     document.getElementById("2").innerHTML == "X" &&
     document.getElementById("3").innerHTML == "X"){
         alert("X won")
+        p1score++;
+        restart();
     }
     if(document.getElementById("4").innerHTML == "X" &&
     document.getElementById("5").innerHTML == "X" &&
     document.getElementById("6").innerHTML == "X"){
         alert("X won")
+        p1score++;
+        restart();
     }
     if(document.getElementById("7").innerHTML == "X" &&
     document.getElementById("8").innerHTML == "X" &&
     document.getElementById("9").innerHTML == "X"){
         alert("X won")
+        p1score++;
+        restart();
     }
     if(document.getElementById("1").innerHTML == "X" &&
     document.getElementById("5").innerHTML == "X" &&
     document.getElementById("9").innerHTML == "X"){
         alert("X won")
+        p1score++;
+        restart();
     }
     if(document.getElementById("3").innerHTML == "X" &&
     document.getElementById("5").innerHTML == "X" &&
     document.getElementById("7").innerHTML == "X"){
         alert("X won")
+        p1score++;
+        restart();
     }
     if(document.getElementById("1").innerHTML == "X" &&
     document.getElementById("2").innerHTML == "X" &&
     document.getElementById("3").innerHTML == "X"){
         alert("X won")
+        p1score++;
+        restart();
     }
     if(document.getElementById("1").innerHTML == "X" &&
     document.getElementById("4").innerHTML == "X" &&
     document.getElementById("7").innerHTML == "X"){
         alert("X won")
+        p1score++;
+        restart();
     }
     if(document.getElementById("2").innerHTML == "X" &&
     document.getElementById("5").innerHTML == "X" &&
     document.getElementById("8").innerHTML == "X"){
         alert("X won")
+        p1score++;
+        restart();
     }
     if(document.getElementById("3").innerHTML == "X" &&
     document.getElementById("6").innerHTML == "X" &&
     document.getElementById("9").innerHTML == "X"){
         alert("X won")
+        p1score++;
+        restart();
     }
-    
     if(document.getElementById("1").innerHTML == "O" &&
     document.getElementById("2").innerHTML == "O" &&
     document.getElementById("3").innerHTML == "O"){
         alert("O won")
+        p2score++;
+        restart();
     }
     if(document.getElementById("4").innerHTML == "O" &&
     document.getElementById("5").innerHTML == "O" &&
     document.getElementById("6").innerHTML == "O"){
         alert("O won")
+        p2score++;
+        restart();
     }
     if(document.getElementById("7").innerHTML == "O" &&
     document.getElementById("8").innerHTML == "O" &&
     document.getElementById("9").innerHTML == "O"){
         alert("O won")
+        p2score++;
+        restart();
     }
     if(document.getElementById("1").innerHTML == "O" &&
     document.getElementById("5").innerHTML == "O" &&
     document.getElementById("9").innerHTML == "O"){
         alert("O won")
+        p2score++;
+        restart();
     }
     if(document.getElementById("3").innerHTML == "O" &&
     document.getElementById("5").innerHTML == "O" &&
     document.getElementById("7").innerHTML == "O"){
         alert("O won")
+        p2score++;
+        restart();
     }
     if(document.getElementById("1").innerHTML == "O" &&
     document.getElementById("2").innerHTML == "O" &&
     document.getElementById("3").innerHTML == "O"){
         alert("O won")
+        p2score++;
+        restart();
     }
     if(document.getElementById("1").innerHTML == "O" &&
     document.getElementById("4").innerHTML == "O" &&
     document.getElementById("7").innerHTML == "O"){
         alert("O won")
+        p2score++;
+        restart();
     }
     if(document.getElementById("2").innerHTML == "O" &&
     document.getElementById("5").innerHTML == "O" &&
     document.getElementById("8").innerHTML == "O"){
         alert("O won")
+        p2score++;
+        restart();
     }
     if(document.getElementById("3").innerHTML == "O" &&
     document.getElementById("6").innerHTML == "O" &&
     document.getElementById("9").innerHTML == "O"){
         alert("O won")
+        p2score++;
+        restart();
     }
 }
 
@@ -121,7 +162,7 @@ function getPlayer(counter){
     if(counter%2 == 0){
         return "X"
     }else{
-        return "0"
+        return "O"
     }
 }
 
@@ -131,4 +172,25 @@ function restart(){
         document.getElementById(id).innerHTML = "?" 
     })
     counter = 0;
+    tiecount=0;
+}
+
+function tie(){
+    var ids = ["1","2","3","4","5","6","7","8","9"]
+    ids.map((id)=>{
+        if(document.getElementById(id).innerHTML != "?"){
+            tiecount++;
+        } 
+    })
+    if(tiecount>=45){
+        alert("game tied");
+        tiescore+=1;
+        restart();  
+    }
+}
+
+function scoreupdate(){
+    document.getElementById("p1sc").innerHTML = p1score;
+    document.getElementById("p2sc").innerHTML = p2score;
+    document.getElementById("tiesc").innerHTML = tiescore;
 }
