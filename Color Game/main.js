0468cccd52f0e681856b44e909649efaca759622
unknown <ikanishkb@gmail.com>
2019-06-28 01:53:11 +0530

    random color picking added

--- Color Game/main.js
+++ Color Game/main.js
@@ -8,19 +8,34 @@ var colors = [
 ]
 
 var squares = document.getElementsByClassName("square");
-var pickedcolor = colors[3];    
+var pickedcolor = pickColor();
+var messageDisplay = document.getElementById("message");
+var colorDisplay = document.getElementById("rgb");
+colorDisplay.textContent = pickedcolor;
 
 for(var i=0;i<squares.length;i++){
     squares[i].style.backgroundColor = colors[i];
     squares[i].addEventListener("click",function(){
         var clickedcolor = this.style.backgroundColor;
         if(clickedcolor === pickedcolor){
-            alert("correct");
+            messageDisplay.textContent = "Correct guess!"
+            changeColor(clickedcolor);
         }
         else{
-            alert("wrong");
+            this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = "Try Again!"
         }
     });
 }
 
-document.getElementById("rgb").innerHTML = pickedcolor;
+function changeColor(color){
+    for(var i=0; i<squares.length; i++){
+        squares[i].style.backgroundColor = color;
+    }
+}
+
+function pickColor(){
+    var random = Math.floor(Math.random() * colors.length);
+    console.log(random);
+    return colors[random];
+}
