f0bdaaa4db4aec95abf4ef5be407018e40ef670e
unknown <ikanishkb@gmail.com>
2019-06-28 02:10:17 +0530

    generate random color for array

--- Color Game/main.js
+++ Color Game/main.js
@@ -1,17 +1,10 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(255, 0, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(0, 255, 0)",
-    "rgb(255, 255, 255)"
-]
-
+var colors = generateRandomColors(6);
 var squares = document.getElementsByClassName("square");
 var pickedcolor = pickColor();
 var messageDisplay = document.getElementById("message");
 var colorDisplay = document.getElementById("rgb");
 colorDisplay.textContent = pickedcolor;
+var h1 = document.querySelector("h1");
 
 for(var i=0;i<squares.length;i++){
     squares[i].style.backgroundColor = colors[i];
@@ -20,6 +13,7 @@ for(var i=0;i<squares.length;i++){
         if(clickedcolor === pickedcolor){
             messageDisplay.textContent = "Correct guess!"
             changeColor(clickedcolor);
+            h1.style.backgroundColor = clickedcolor;
         }
         else{
             this.style.backgroundColor = "#232323";
@@ -38,4 +32,21 @@ function pickColor(){
     var random = Math.floor(Math.random() * colors.length);
     console.log(random);
     return colors[random];
+}
+
+function generateRandomColors(num){
+    var arr=[];
+    for(var i=0; i<num; i++){
+        arr.push(randomColors());
+    }
+    return arr;
+}
+
+function randomColors(){
+    var r = Math.ceil(Math.random()*255);
+    var g = Math.ceil(Math.random()*255);
+    var b = Math.ceil(Math.random()*255);
+    var string = `rgb(${r}, ${g}, ${b})`;
+    console.log(string);
+    return string;
 }
