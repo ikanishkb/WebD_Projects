8f1893c700cdae38f0d9941c252b4d1ec290f09b
unknown <ikanishkb@gmail.com>
2019-07-19 00:15:02 +0530

    Routing added

--- react_routing/src/index.js
+++ react_routing/src/index.js
@@ -2,19 +2,26 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import App from './App.js';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Tictactoe from './tictactoe';
+import Calculator from './calculator';
+import Home from './Home';
 
-class Index extends Component {
+class App extends Component {
     render() {
         return (
             <div>
-                <App/>
+                <BrowserRouter>
+                    <Route exact path="/" component={Home}/>
+                    <Route exact path="/TicTacToe" component={Tictactoe}/>
+                    <Route exact path="/Calculator" component={Calculator}/>
+                </BrowserRouter>
             </div>
         );
     }
 }
 
-ReactDOM.render(<Index />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
