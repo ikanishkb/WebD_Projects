048d0db8bc5d17ebcc2b5f2aea2f5e1f67210a4e
KANISHK BATRA <batrakanishk29@gmail.com>
2019-07-19 00:39:55 +0530

    Home updated

--- react_routing/src/Home.js
+++ react_routing/src/Home.js
@@ -5,6 +5,8 @@ export default class Home extends Component {
         return (
             <div>
                 <h1>React Routing used!!</h1>
+                <h2>use "/Calculator" in Address bar to go to calculator</h2>
+                <h2>use "/Tictactoe" in Address bar to go to Tictactoe</h2>
             </div>
         )
     }
