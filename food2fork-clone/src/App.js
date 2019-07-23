861905a1f4faebd7a6a1714be404b6a1e4c7379b
unknown <ikanishkb@gmail.com>
2019-07-23 09:26:21 +0530

    first commit

---
+++ food2fork-clone/src/App.js
@@ -0,0 +1,15 @@
+import React, {Fragment} from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <Fragment>
+      <div>
+        Hello frm app
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
